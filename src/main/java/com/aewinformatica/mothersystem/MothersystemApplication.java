ac8979597583ac458fc445c3d3ca8c76ad33cb6b
gilsonlucas <gilsonlucasjs@gmail.com>
2019-05-24 18:49:28 -0300

    Projeto Basico

---
+++ src/main/java/com/aewinformatica/mothersystem/MothersystemApplication.java
@@ -0,0 +1,13 @@
+package com.aewinformatica.mothersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MothersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MothersystemApplication.class, args);
+	}
+
+}
